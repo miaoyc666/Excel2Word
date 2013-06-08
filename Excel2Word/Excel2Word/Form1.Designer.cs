8993f5dea07ccf4e42bde6c09f5de09329c5d043
myc <miaoyc1989@hotmail.com>
2013-06-08 09:23:09 +0800

    *ADD* 添加项目：读取Excel中内容并将其转换到WORD中
    Signed-off-by: myc <miaoyc1989@hotmail.com>

---
+++ Excel2Word/Excel2Word/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows.Forms;
+namespace WindowsFormsApplication1
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.openFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "open file";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // openFileDlg
+            // 
+            this.openFileDlg.Filter = "Excel文件 (*.xml;*.xls;*.xlsx;*.xlsm;*.xlsb)|*.xml;*.xls;*.xlsx;*.xlsm;*.xlsb|所有文件(*.*)|*.*";
+            this.openFileDlg.InitialDirectory = "c:\\";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 379);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "TestForm";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.OpenFileDialog openFileDlg;
+    }
+}
+
---
+++ Excel2Word/Excel2Word/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections.Generic;
+
+namespace WindowsFormsApplication1
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            DataSet excelData = ImportFromExcel();
+
+            if (0 == excelData.Tables.Count)
+            {
+                return;
+            }
+            
+            int asd = excelData.Tables[0].Rows.Count;       // 取第一个表中的数据
+            List<int> test = new List<int>();
+
+            
+            for ( int index = 0; index < 10; ++index )
+            {
+                test.Insert(index, index);
+            }
+            for ( int index = 0; index < test.Count; ++index )
+            {
+            }
+        }
+
+
+
+        #region 从Excel文件导入到DataSet
+        
+        //        /// <summary>
+        //        /// 从Excel导入文件
+        //        /// </summary>
+        //        /// <param name="strExcelFileName">Excel文件名</param>
+        //        /// <returns>返回DataSet</returns>
+        //        public DataSet ImportFromExcel(string strExcelFileName)
+        //        {
+        //            return doImport(strExcelFileName);
+        //        }
+        /// <summary>
+        /// 从选择的Excel文件导入
+        /// </summary>
+        /// <returns>DataSet</returns>
+        public DataSet ImportFromExcel()
+        {
+            DataSet dataSet = new DataSet();
+            if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dataSet = doImport(openFileDlg.FileName);
+            }
+            return dataSet;
+        }
+
+        /// <summary>
+        /// 从指定的Excel文件导入
+        /// </summary>
+        /// <param name="strFileName">Excel文件名</param>
+        /// <returns>DataSet</returns>
+        public DataSet ImportFromExcel(string strFileName)
+        {
+            DataSet dataSet = new DataSet();
+            dataSet = doImport(strFileName);
+            return dataSet;
+        }
+
+        /// <summary>
+        /// 执行导入
+        /// </summary>
+        /// <param name="strFileName">文件名</param>
+        /// <returns>DataSet</returns>
+        private DataSet doImport(string strFileName)
+        {
+            if (strFileName == "")
+            {
+                return null;
+            }
+
+            string ExcelTableName = "";
+            string strConn = "Provider=Microsoft.Ace.OleDb.12.0;" +
+                "Data Source=" + strFileName + ";" +
+                "Extended Properties='Excel 8.0;HDR=NO;IMEX=1';";
+
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            OleDbDataAdapter myCommand;
+            //获取文件中TABLE类型的表
+            DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            DataSet dsExcel = new DataSet();
+            try
+            {
+                DataRow dr = schemaTable.Rows[0];
+                ExcelTableName = dr["TABLE_NAME"].ToString().Trim();
+                //从对应Excel内容的表中获取数据
+                string strExcel = "select * from [" + ExcelTableName + "]";
+                myCommand = new OleDbDataAdapter(strExcel, strConn);
+                myCommand.Fill(dsExcel);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dsExcel;
+        }
+        #endregion
+    }
+}
